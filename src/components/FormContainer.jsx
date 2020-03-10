cc0bcf1ab459275dd3eacf30f01cbd1f4478e87d
Ariel Villarreal <arielskap@gmail.com>
2020-03-10 14:35:53 -0300

    Diseño login

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { ipcRenderer } from 'electron';
 import { login } from '../renderer-process';
-import ExitModal from './ExitModal';
+import FormDetails from './FormDetails';
 import animateCSS from '../funciones';
-import '../assets/styles/Form.scss';
+import ver from '../assets/static/ver.svg';
+import noVer from '../assets/static/noVer.svg';
 
-const Form = () => {
+const FormContainer = () => {
   const [information, setInformation] = useState('');
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const history = useHistory();
 
   const handleCloseModal = () => {
-    setModalIsOpen(false);
+    animateCSS('.Modal__container', 'slideOutUp faster', () => {
+      setModalIsOpen(false);
+      document.querySelector('.button_submit_Form').removeAttribute('disabled');
+      document.querySelector('.button_submit_Form').classList.remove('opacity-50', 'cursor-not-allowed');
+      setInformation(`⚠ ${message} ⚠`);
+    });
+    animateCSS('.Modal', 'fadeOut faster');
   };
 
   const handleOpenModal = () => {
@@ -20,7 +27,9 @@ const Form = () => {
   };
 
   const handlePasswordExpired = () => {
-    history.push('/expired');
+    animateCSS('.Form', 'fadeOut faster', () => {
+      history.push('/expired');
+    });
   };
 
   const handleSubmit = (e) => {
@@ -76,43 +85,36 @@ const Form = () => {
     });
   };
 
+  const handleSwitchVisiblePass = () => {
+    const inputPass = document.querySelector('#password');
+    if (inputPass.getAttribute('type') === 'password') {
+      inputPass.setAttribute('type', 'text');
+      document.querySelector('.img__visiblePass').src = noVer;
+    } else {
+      inputPass.setAttribute('type', 'password');
+      document.querySelector('.img__visiblePass').src = ver;
+    }
+  };
   useEffect(() => {
     document.getElementById('username').focus();
   }, []);
 
   return (
     <>
-      <form className='Form shadow-md rounded px-8 pt-6 pb-8 mb-4 animated fadeIn' onSubmit={handleSubmit}>
-        <Link to='/home'>
-          Go to Home
-        </Link>
-        <div className='mb-4'>
-          <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='username'>
-            Usuario:
-            <input autoComplete='username' className='shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline' name='username' id='username' type='text' placeholder='Usuario' tabIndex={0} />
-          </label>
-        </div>
-        <div className='mb-6'>
-          <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='password'>
-            Contraseña:
-            <input autoComplete='current-password' className='shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline' name='contraseña' id='password' type='password' placeholder='Ingrese Contraseña' tabIndex={0} />
-          </label>
-        </div>
-        <div className='div_information text-center text-red-500 text-xs my-4'>
-          <p className='p_information'>{information}</p>
-        </div>
-        <div className='flex items-center justify-center'>
-          <button className='button_submit_Form bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type='submit' tabIndex={0}>
-            Iniciar Sesión
-          </button>
-        </div>
-        <div className='pt-5 text-right'>
-          <button type='button' className='text-gray-600 text-xs italic hover:text-blue-600 py-4' tabIndex={0}>¿Olvidó su contraseña?</button>
-        </div>
-      </form>
-      <ExitModal isOpen={modalIsOpen} onCloseModal={handleCloseModal} onConfirm={handlePasswordExpired}>⚠ ¡Se vencio la constraseña! ⚠</ExitModal>
+      <FormDetails
+        information={information}
+        modalIsOpen={modalIsOpen}
+        handles={
+          {
+            handleCloseModal,
+            handlePasswordExpired,
+            handleSubmit,
+            handleSwitchVisiblePass,
+          }
+        }
+      />
     </>
   );
 };
 
-export default Form;
+export default FormContainer;
