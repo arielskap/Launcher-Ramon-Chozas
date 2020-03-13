b21ee944f104c58314f830c4350843a07047de84
Fiorella Bissi <fiorellabissi@hotmail.com>
2020-03-13 12:45:04 -0300

    mejoras

--- src/components/ForgetPassContainer.jsx
+++ src/components/ForgetPassContainer.jsx
@@ -3,6 +3,7 @@ import '../assets/styles/ForgetPass.scss';
 import ForgetPassDetails from './ForgetPassDetails';
 
 const ForgetPassContainer = () => {
+
   const handleChange = () => {
     if (document.querySelector('#username').value && document.querySelector('#dni').value) {
       document.querySelector('#btnForm').classList.remove('opacity-50', 'cursor-not-allowed');
@@ -29,6 +30,10 @@ const ForgetPassContainer = () => {
       buttonText: 'Restaurar',
       handleSubmit,
     });
+    document.querySelector('.step1').classList.replace('bg-gray-700', 'bg-gray-500');
+    document.querySelector('.step2').classList.replace('bg-gray-500', 'bg-gray-700');
+    document.querySelector('#btnForm').setAttribute('disabled', '');
+    document.querySelector('#btnForm').classList.add('opacity-50', 'cursor-not-allowed');
   };
 
   const [data, setData] = useState({
@@ -44,22 +49,17 @@ const ForgetPassContainer = () => {
 };
 
 export default ForgetPassContainer;
-/*
-<h2>Paso 2 - Confirmar </h2>
-        <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='username'>
-          Usuario:
-          <input onChange={handleChange2} className='mt-1 h-10 shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline' autoComplete='username' type='text' name='username' id='username' placeholder='Ingresar Usuario' />
-        </label>
-        <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='dni'>
-          DNI:
-          <input onChange={handleChange2} className='mt-1 h-10 shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline' autoComplete='dni' type='text' name='dni' id='dni' placeholder='Ingrese el DNI' />
-        </label>
 
-        <div className='flex items-center justify-center'>
-          <button type='submit' id='prevBtn' className='button_submit_Form bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline opacity-50 cursor-not-allowed'>Restaurar</button>
-        </div>
-      </div>
-      <div id='circles' className='flex item-center justify-center pt-6'>
-        <span className='step1' />
-        <span className='step2' />
-      </div>*/
+// if (document.querySelector('#username').value && document.querySelector('#dni').value && document.querySelector('#supervisor').value) {
+//   document.querySelector('#btnForm').classList.remove('opacity-50', 'cursor-not-allowed');
+//   document.querySelector('#btnForm').removeAttribute('disabled');
+// } else {
+//   if (!document.querySelector('#btnForm').hasAttribute('disabled')) {
+//     document.querySelector('#btnForm').setAttribute('disabled', '');
+//     document.querySelector('#btnForm').classList.add('opacity-50', 'cursor-not-allowed');
+//   }
+// }
+// if (document.querySelector('.step1').classList.contains('bg-gray-700')) {
+//   document.querySelector('.step1').classList.replace('bg-gray-700, bg-gray-500');
+//   document.querySelector('.step2').classList.replace('bg-gray-500, bg-gray-700');
+// }
--- src/components/ForgetPassDetails.jsx
+++ src/components/ForgetPassDetails.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const ForgetPassDetails = ({ title, hasSupervisor, buttonText, handleChange, handleSubmit }) => {
+
+  const Combobox = () => {
+    if (document.querySelector('#supervisor').value) {
+      document.querySelector('#btnForm').classList.remove('opacity-50', 'cursor-not-allowed');
+      document.querySelector('#btnForm').removeAttribute('disabled');
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit} id='regForm' className='Form border-solid border-2 border-white rounded-lg shadow-2xl px-8 pt-6 pb-8 mb-4 animated fadeIn'>
       <div className='tab'>
@@ -16,21 +24,21 @@ const ForgetPassDetails = ({ title, hasSupervisor, buttonText, handleChange, han
         {hasSupervisor && (
           <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='supervisor'>
             Supervisor:
-            <select onChange={handleChange} className='block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline' id='supervisor' name='supervisor' required>
+            <select onChange={Combobox} className='block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline' id='supervisor' name='supervisor' required>
               <option value=''>--Supervisor--</option>
               <option value='Alfredo Guillot'>Alfredo Guillot</option>
               <option value='Ariel Villareal'>Ariel Villareal</option>
-              <option value='Abbul'>Abbul Rodriguez</option>
+              <option value='Abbul Rodriguez'>Abbul Rodriguez</option>
             </select>
           </label>
         )}
         <div className='flex items-center justify-center'>
-          <button type='submit' id='btnForm' className='button_submit_Form bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline opacity-50 cursor-not-allowed' disabled>{buttonText}</button>
+          <button type='submit' id='btnForm' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline opacity-50 cursor-not-allowed' disabled>{buttonText}</button>
         </div>
       </div>
       <div id='circles' className='flex item-center justify-center pt-6'>
-        <span className='step1' />
-        <span className='step2' />
+        <span className='step1 bg-gray-700' />
+        <span className='step2 bg-gray-500' />
       </div>
     </form>
   );
